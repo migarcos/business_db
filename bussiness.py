8b0c52276a007e9a9863ddfbf747d897180178a5
migarcos <migark@hotmail.com>
2026-07-30 17:26:52 -0500

    init employee list added

--- bussiness.py
+++ bussiness.py
@@ -18,11 +18,24 @@ def init_db():
             ("Operations", "OPS")
         ]
 
+        init_empl = [
+            ("Aneth", "Wall", "awall@buss.com", "2023-01-15", 3500.00, "HR"),
+            ("Kyle", "Bowser", "kbowser@buss.com", "2024-01-15", 4200.00, "ACC"),
+            ("Te Ara", "Erueti", "terueti@buss.com", "2025-01-01", 3900.00, "ACC"),
+            ("Garret", "Peterson", "gpeterson@buss.com", "2023-06-15", 2000.00, "SAL"),
+            ("Percy", "Brandot", "pbrandot@buss.com", "2024-07-01", 2000.00, "SAL"),
+            ("Salma", "Hayek", "shayek@buss.com", "2023-01-15", 3000.00, "SAL"),
+            ("Angeline", "Jolie", "ajolie@buss.com", "2024-02-01", 2000.00, "SAL"),
+            ("Sandra", "Bullock", "sbullock@buss.com", "2025-09-15", 2000.00, "SAL"),
+            ("Kevin", "Space", "kspace@buss.com", "2023-11-15", 3500.00, "OPS"),
+            ("Darth", "Vader", "dvader@buss.com", "2026-01-02", 3500.00, "OPS")
+        ]
+
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS departments (
                 department_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 department_name TEXT NOT NULL,
-                department_code TEXT UNIQuE NOT NULL
+                department_code TEXT UNIQUE NOT NULL
             )    
         """)
 
@@ -39,10 +52,16 @@ def init_db():
                 email TEXT NOT NULL,
                 hire_date DATE NOT NULL,
                 status TEXT DEFAULT 'Active',
+                salary DECIMAL(10, 2) NOT NULL CHECK (salary > 0),
                 department_id INT,
-                FOREIGN KEY (department_id) REFERENCES departments(deparment_id)
-            )    
+                FOREIGN KEY (department_id) REFERENCES departments(department_id)
+            )
         """)
+
+        cursor.executemany("""
+            INSERT OR IGNORE INTO employee (fname, lname, email, hire_date, salary, department_id)
+            VALUES (?, ?, ?, ?, ?, (SELECT department_id FROM departments WHERE department_code = ?) )
+        """, init_empl)
 
         conn.commit()
 
@@ -55,11 +74,31 @@ def show_departments():
         print("\n --- DEPARTMENTS LIST --- ")
         for row in rows:
             print(f'ID: {row['department_id']} | Código: {row['department_code']} | Nombre: {row['department_name']}')
+        print("\n")
+
+def show_init_empl():
+    with get_conn() as conn:
+            cursor = conn.cursor()
+            # cursor.execute("SELECT * FROM employee")
+            cursor.execute("""SELECT
+                        e.fname, e.lname, e.email, e.hire_date, e.salary, d.department_code 
+                        FROM employee e
+                        JOIN departments d ON e.department_id = d.department_id
+                        ORDER BY d.department_code ASC
+            """)
+
+            rows = cursor.fetchall()
+            print("\n --- EMPLOYEES LIST --- ")
+            print(f"\n{'Name':<10} {'Last':<12} {'Email':<28} {'Hire Date':<12} {'Salary':>10} {'Dept':>6}")
+            print("-" * 82)
+            for row in rows:
+                print(f'{row['fname']:<10} {row['lname']:<12} {row['email']:<28} {row['hire_date']:<12} {row['salary']:>10.2f} {row['department_code']:>6}')
 
 # --- CLI handlers ---
 def main():
     init_db()
     show_departments()
+    show_init_empl()
 
 if __name__ == "__main__":
     main()
